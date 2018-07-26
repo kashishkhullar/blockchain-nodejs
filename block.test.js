f903332da7bd76f76b9aade3094e3c07f2d69c64
kashishkhullar <kkhullar7@gmail.com>
2018-07-26 19:10:12 +0530

    added block tests

---
+++ block.test.js
@@ -0,0 +1,38 @@
+const Block = require('./block');
+
+/**
+ * describe is jest specific function
+ * @prarams
+ * name of the object as string for which the test is written
+ * function that will define a series of tests
+ */
+describe("Block",()=>{
+
+    let data,lastBlock,block;
+    /**
+     * beforeEach allows us to run some code before
+     * running any test
+     * example creating an instance
+     */
+    beforeEach(()=>{
+         data = 'bar';
+         lastBlock = Block.genesis();
+         block = Block.mineBlock(lastBlock,data);
+    });
+    /**
+     * it function is used to write unit tests
+     * first param is a description
+     * second param is callback arrow function
+     */
+    it("sets the `data` to match the input",()=>{
+        /**
+         * expect is similar to assert
+         * it expects something
+         */
+        expect(block.data).toEqual(data);
+    });
+
+    it("sets the `lastHash` to matc the hash of the last block",()=>{
+        expect(block.lastHash).toEqual(lastBlock.hash);
+    });
+})
