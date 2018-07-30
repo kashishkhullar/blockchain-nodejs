9cd3440e1dd24b3f65b15eb9a9b7be8ce758c78d
kashishkhullar <kkhullar7@gmail.com>
2018-07-30 15:59:02 +0530

    replace new chain

--- blockchain.js
+++ blockchain.js
@@ -35,6 +35,18 @@ class Blockchain{
         return true;
 
     }
+
+    replaceChain(newChain){
+        if(newChain.length <= this.chain.length){
+            console.log("Recieved chain is not longer than the current chain");
+            return;
+        }else if(!this.isValidChain(newChain)){
+            console.log("Recoived chain is invalid");
+        }
+        
+        console.log("Replacing the current chain with new chain");
+        this.chain = newChain; 
+    }
 }
 
 module.exports = Blockchain;
