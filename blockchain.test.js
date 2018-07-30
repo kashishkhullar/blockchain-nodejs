34a07981dfc7079ffa66420c1d2aa0fac4742884
kashishkhullar <kkhullar7@gmail.com>
2018-07-30 16:46:53 +0530

    replace chain tests

--- blockchain.test.js
+++ blockchain.test.js
@@ -37,4 +37,17 @@ describe("Blockchain",()=>{
         expect(blockchain.isValidChain(blockchain2.chain)).toBe(false);
     })
 
+    it('replaces the chain with a valid chain',()=>{
+        blockchain2.addBlock('goo');
+        blockchain.replaceChain(blockchain2.chain);
+        expect(blockchain.chain).toEqual(blockchain2.chain);
+    });
+
+    it('does not replaces the chain with a one with less than or equal to chain',()=>{
+        blockchain.addBlock('foo');
+        blockchain.replaceChain(blockchain2.chain);
+        expect(blockchain.chain).not.toEqual(blockchain2.chain);
+    });
+    
+
 })
