139fb01df46273753b6624174f1ccbc169e22f82
kashishkhullar <kkhullar7@gmail.com>
2018-07-30 17:55:52 +0530

    updated p2p server class

--- app/p2p-server.js
+++ app/p2p-server.js
@@ -12,14 +12,43 @@ class P2pserver{
         this.sockets = [];
     }
 
+    // create a new p2p server and connections
+
     listen(){
+        // create the p2p server with port as argument
         const server = new WebSocket.Server({ port: P2P_PORT });
+
+        // event listener and a callback function for any new connection
         server.on('connection',socket => this.connectSocket(socket));
+
+        // to connect to the peers that we have specified
+        this.connectToPeers();
+
         console.log(`Listening for peer to peer connection on port : ${P2P_PORT}`);
     }
-    
+
+    //push the new socket to the sockets array
     connectSocket(socket){
         this.sockets.push(socket);
         console.log("Socket connected");
     }
+
+    connectToPeers(){
+
+        //connect to each peer
+        peers.forEach(peer => {
+
+            // create a socket for each peer
+            const socket = new WebSocket(peer);
+            
+            // open event listner is emitted when a connection is established
+            // saving the socket in the array
+            socket.on('open',() => this.connectSocket(socket));
+
+        });
+    }
+
+
 }
+
+module.exports = P2pserver;
