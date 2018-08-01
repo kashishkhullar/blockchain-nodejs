cbaecad6ba913ac2b6ec0cb69d0fec959e2f8f7e
kashishkhullar <kkhullar7@gmail.com>
2018-08-01 21:58:29 +0530

    added transaction end points

--- app/index.js
+++ app/index.js
@@ -2,8 +2,12 @@ const express = require('express');
 const Blockchain = require('../blockchain');
 const bodyParser = require('body-parser');
 const P2pserver = require('./p2p-server');
+
 //get the port from the user or set the default port
 const HTTP_PORT = process.env.HTTP_PORT || 3001;
+
+const Wallet = require('../wallet');
+const TransactionPool = require('../wallet/transaction-pool');
 
 //create a new app
 const app  = express();
@@ -13,6 +17,13 @@ app.use(bodyParser.json());
 
 // create a new blockchain instance
 const blockchain = new Blockchain();
+
+// create a new wallet
+const wallet = new Wallet();
+
+// create a new transaction pool which will be later
+// decentralized and synchronized using the peer to peer server
+const transactionPool = new TransactionPool();
 
 // create a p2p server instance with the blockchain
 const p2pserver = new P2pserver(blockchain);
@@ -37,6 +48,11 @@ app.post('/mine',(req,res)=>{
      */
     p2pserver.syncChain();
     res.redirect('/blocks');
+});
+
+// api to view transaction in the transaction pool
+app.get('/transactions',(req,res)=>{
+    res.json(transactionPool.transactions);
 })
 
 // app server configurations
